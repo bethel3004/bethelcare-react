0c483c6e91060b7cd7dd0fff23e269782a7ed292
bethel3004 <bethelo.net@gmail.com>
2026-04-29 07:21:50 +0900

    fix sheet name to 혈당혈압

--- src/pages/BpSugar.js
+++ src/pages/BpSugar.js
@@ -37,7 +37,7 @@ export default function BpSugar({ db }) {
     const newId = String(Math.max(0,...bp.map(x=>parseInt(x.ID)||0))+1);
     const newBp = { ...form, ID:newId, 입소자ID:p?.ID||'' };
     setBp([...bp, newBp]);
-    appendToSheet('혈당/혈압', newBp);
+    appendToSheet('혈당혈압', newBp);
     setForm({ 성명:'', 날짜:'', 혈당:'', 혈압수축기:'', 혈압이완기:'', 비고:'' });
     setTab('list');
   };
